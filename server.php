378682edfd58afcbfeff63395b9b7fa5da36c6b5
Unknown <amelie.ar98@gmail.com>
2019-01-11 10:58:44 +0100

    comm

--- server.php
+++ server.php
@@ -1,23 +1,22 @@
 <?php
 // session_start();
-// initializing variables
 $username = "";
 $email    = "";
 $errors = array();
 
-// connect to the database
+// connexion à la bdd
 $db = mysqli_connect('localhost', 'root', 'Password123!', 'backend');
 
-// REGISTER USER
+// enregistrer un utilisateur
 if (isset($_POST['reg_user'])) {
-  // receive all input values from the form
+  // recevoir les info du formulaire
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
+  // vérifie que le formulaire est bien rempli 
+  // array push pour les erreurs correspondantes 
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
   if (empty($password_1)) { array_push($errors, "Password is required"); }
@@ -25,13 +24,13 @@ if (isset($_POST['reg_user'])) {
 	array_push($errors, "The two passwords do not match");
   }
 
-  // first check the database to make sure
-  // a user does not already exist with the same username and/or email
+  
+  // vérifie dans la bdd si les informations ne sont pas déjà dans le serveur
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
 
-  if ($user) { // if user exists
+  if ($user) { // si user existe déjà
     if ($user['username'] === $username) {
       array_push($errors, "Ton pseudo est déjà pris");
     }
@@ -41,15 +40,15 @@ if (isset($_POST['reg_user'])) {
     }
   }
 
-  // Finally, register user if there are no errors in the form
+  // si il n'y a pas d'erreur, enregistre l'utilisateur 
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
-
-  	$query = "INSERT INTO users (username, email, password)
+  	$password = md5($password_1);//script le mot de passe avant de l'enregistrer dans la bdd
+  	
+    $query = "INSERT INTO users (username, email, password)
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
+  	$_SESSION['success'] = "connecté";
   	header('location: index.php');
   }
 }
@@ -61,10 +60,10 @@ if (isset($_POST['login_user'])) {
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($username)) {
-  	array_push($errors, "Username is required");
+  	array_push($errors, "nom d'utilisateur requis");
   }
   if (empty($password)) {
-  	array_push($errors, "Password is required");
+  	array_push($errors, "mot de passe requis");
   }
 
   if (count($errors) == 0) {
@@ -73,10 +72,10 @@ if (isset($_POST['login_user'])) {
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
+  	  $_SESSION['success'] = "connecté";
   	  header('location: index.php');
   	}else {
-  		array_push($errors, "Wrong username/password combination");
+  		array_push($errors, "mauvais mot de passe/nom d'utilisateur");
   	}
   }
 }
